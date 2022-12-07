76afd61d9ad2a22bfeeb4f60a22560e9bdd41355
tam201197 <tam201197@gmail.com>
2022-12-07 15:56:29 +0100

    3. commit

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -14,15 +14,19 @@ object Test {
   val projectJAR = "C:/Users/tam20/SoftwareDevelopmentTool/src/main/jar-files/app-debug.jar"
   val projectJAR2 = "C:/Users/tam20/SoftwareDevelopmentProject/src/main/jar-files/STD_app.jar"
   val project = Project(new File(projectJAR2))
+  var result = ""
 
   def checkMethod(methods_of_summary: List[MethodSummary]) : Unit = {
     project.allMethodsWithBody.foreach(method => {
-      println(method.name)
-      println("method length: " + method.parameterTypes.length)
+      val parameterLength = method.parameterTypes.length
       val signature = method.signature.toJava
-      val parameters = signature.substring(signature.lastIndexOf("(")+1, signature.lastIndexOf(")"))
-      println("return type: " + method.returnType.toJava)
-      println("end method")
+      var parameters = signature.substring(signature.lastIndexOf("(")+1, signature.lastIndexOf(")"))
+      if(parameters == "")
+        parameters="null"
+      val returnType = method.returnType.toJava
+      result = returnType + " " + parameterLength + " " + parameters
+      if (methods_of_summary.exists(p => p.toResult().equals(result)))
+        println("find match: " + method.name)
     })
   }
 
