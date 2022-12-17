a2213859738ac5ade2bafc9c9607111eecdc4e6c
forvev <zel1916@o2.pl>
2022-12-17 23:28:37 +0100

    Finding pattern and new jar file

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -10,28 +10,37 @@ import java.io.File
 import scala.xml.NodeSeq.seqToNodeSeq
 
 object Test {
-  val xml_url = "C:/Users/tam20/SoftwareDevelopmentProject/src/main/xml-files/PatternMatcher.xml"
-  val projectJAR = "C:/Users/tam20/SoftwareDevelopmentTool/src/main/jar-files/app-debug.jar"
-  val projectJAR2 = "C:/Users/tam20/SoftwareDevelopmentProject/src/main/jar-files/STD_app.jar"
+  val xml_url = "C:/Users/Artur/Desktop/studia/V semestr/Software Development Tools/Scala_project_2/src/main/xml-files/PatternMatcher.xml"
+  val projectJAR = "C:/Users/Artur/Desktop/studia/V semestr/Software Development Tools/Scala_project_2/src/main/jar-files/app-debug.jar"
+  val projectJAR2 = "C:/Users/Artur/Desktop/studia/V semestr/Software Development Tools/Scala_project_2/src/main/jar-files/STD_app2.jar"
   val project = Project(new File(projectJAR2))
   var result = ""
 
   def checkMethod(methods_of_summary: List[MethodSummary]) : Unit = {
-    project.allMethodsWithBody.foreach(method => {
-      val parameterLength = method.parameterTypes.length
+    project.allProjectClassFiles.foreach(specific_class =>{
 
-      val signature = method.signature.toJava
-      var parameters = signature.substring(signature.lastIndexOf("(")+1, signature.lastIndexOf(")"))
-      if(parameters == "")
-        parameters="null"
-      val returnType = method.returnType.toJava
-      result = returnType + " " + parameterLength + " " + parameters
-      if (methods_of_summary.exists(p => p.toResult().equals(result))) {
-        println("find match: " + method.name)
-        println("Of: "+result)
+      val number_of_method = specific_class.methods.length
+      var real_number_of_method: Int = 0
+
+      specific_class.methods.foreach(method =>{
+        val parameterLength = method.parameterTypes.length
+
+        val signature = method.signature.toJava
+        var parameters = signature.substring(signature.lastIndexOf("(") + 1, signature.lastIndexOf(")"))
+        if (parameters == "")
+          parameters = "null"
+        val returnType = method.returnType.toJava
+        result = returnType + " " + parameterLength + " " + parameters
+        if (methods_of_summary.exists(p => p.toResult().equals(result))) real_number_of_method+=1
+      })
+
+      //If the number of methods in the specific class is the same as in the summary...
+      if(real_number_of_method==number_of_method){
+        println("Pattern has been found!\nWith class: "+specific_class.methods)
+
       }
     })
-    //project.allProjectClassFiles.foreach()
+
   }
 
   def main(args: Array[String]): Unit = {
